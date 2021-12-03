f76ebf9779d85c00e5be29a8f9cfdb684550f4fb
qinyuncao <70119413+qinyuncao@users.noreply.github.com>
2021-12-03 16:32:07 -0500

    logo update

--- Code/public/logIn.js
+++ Code/public/logIn.js
@@ -1,5 +1,7 @@
 window.addEventListener('load',async() => {
+
     const curuserid = await (await fetch('/currentuser')).json();
+
     if (curuserid) {
         document.getElementById('homeButton').setAttribute('href','mainPage2.html');
     }
@@ -9,11 +11,13 @@ window.addEventListener('load',async() => {
 });
 
 document.getElementById('loginbutton').addEventListener('click',async() => {
+    
     const inputUsername = document.getElementById('inputUsername').value;
     const inputPassword = document.getElementById('inputPassword').value;
     
     //Insert code to check if this object is in the server. If it is, the user should be logged in
     const response = await (await fetch('/users/login/'+inputUsername+'/'+inputPassword)).json();
+
     //If login succesful, go to main page with user logged in
     if (response) {
         document.getElementById('userUpdate').innerText = 'Correct username and password';
