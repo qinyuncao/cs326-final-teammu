b9d3e851d272346bf87b3e884035edba22aa0796
moizsaqib369 <89888510+moizsaqib369@users.noreply.github.com>
2021-12-07 12:30:16 -0500

    Update server.js

--- Code/server.js
+++ Code/server.js
@@ -38,33 +38,29 @@ app.get('/',async function(req,res){
     }
 });
 
-//Get the current the User 
 app.get('/currentuser',async function(req,res) {
     const result = await client.db("finalProject").collection('currentuser').findOne({'index':0});
     res.end(JSON.stringify(result.userid));
 });
 
-//Get the current hall
 app.get('/currenthall',async function(req,res) {
     const result = await client.db("finalProject").collection('currenthall').findOne({'index':0});
     res.end(JSON.stringify(result.hallid));
 });
 
-//Post the current user to the databse
 app.post('/currentuser',async function(req,res) {
     await client.db("finalProject").collection('currentuser').updateOne({'index':0},{$set: {userid: req.body.user}});
     res.end(JSON.stringify('Current User Updated'));
 });
 
-//Post the current hall to the database
 app.post('/currenthall',async function(req,res) {
     await client.db("finalProject").collection('currenthall').updateOne({'index':0},{$set: {hallid: req.body.hall}});
     res.end(JSON.stringify('Current Hall Updated'));
 });
 
-
 //When client ask for specific user
-//use this when sign up, if the database has the username from front end, return 404
+//use this when sign up
+//*
 app.get('/users/:username',async function(req,res){
     //Check if the database has this username, if not, return 404
     const result = await client.db("finalProject").collection("users").findOne({'username':req.params.username});
@@ -81,6 +77,7 @@ app.get('/users/:username',async function(req,res){
 
 //When client want to create new username and password
 //use this when sign up
+//*
 app.post('/users', async function(req,res){
     const user = {
         email: req.body.email,
@@ -92,7 +89,8 @@ app.post('/users', async function(req,res){
     res.end(JSON.stringify('User Added!'));
 });
 
-//Log in information check, compare the information from client with the information in database.
+//use this when log in
+//*
 app.get('/users/login/:username/:password',async function(req,res){
     const result = await client.db("finalProject").collection("users").findOne({'username':req.params.username,'password':req.params.password});
     if(result){ 
@@ -140,22 +138,6 @@ app.get('/reviewrank',async function(req,res){
     res.send(allHalls.sort((a,b) => b.totalscore - a.totalscore));
 });
 
-//Use this one to post the review has been deleted.
-app.post('/deletereview', async function(req,res) {
-    const deleteReview = req.body.reviewid;
-    await client.db("finalProject").collection('reviews').deleteOne({'reviewid':deleteReview});
-    res.end(JSON.stringify('Review Deleted'));
-});
-
-//Use this one to increase like/dislike 
-app.post('/increaselikedislike', async function(req,res) {
-    const likedislikeobj = {};
-    likedislikeobj[req.body.likedislike] = 1;
-    await client.db("finalProject").collection('reviews').updateOne({'reviewid':req.body.reviewid}, {$inc: likedislikeobj});
-    res.end(JSON.stringify('Review Updated'));
-});
-
-//Use this function to save the whole review page
 app.post('/reviewpage', async function(req,res){
     const reviews = await client.db("finalProject").collection('reviews').find({}).toArray();
     const chosenHall = req.body.hall;
@@ -167,4 +149,17 @@ app.post('/reviewpage', async function(req,res){
     }
     hallReviews = hallReviews.sort((a,b) => (b.likecount + b.dislikecount) - (a.likecount + a.dislikecount));
     res.send(hallReviews.sort((a,b) => (b.likecount - b.dislikecount) - (a.likecount - a.dislikecount)));
+});
+
+app.post('/deletereview', async function(req,res) {
+    const deleteReview = req.body.reviewid;
+    await client.db("finalProject").collection('reviews').deleteOne({'reviewid':deleteReview});
+    res.end(JSON.stringify('Review Deleted'));
+});
+
+app.post('/increaselikedislike', async function(req,res) {
+    const likedislikeobj = {};
+    likedislikeobj[req.body.likedislike] = 1;
+    await client.db("finalProject").collection('reviews').updateOne({'reviewid':req.body.reviewid}, {$inc: likedislikeobj});
+    res.end(JSON.stringify('Review Updated'));
 });
